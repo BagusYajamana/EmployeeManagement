4a2f6754d55f124fb41e422a0aeb1687f278c55a
Bagus Yajamana <93474719+BagusYajamana@users.noreply.github.com>
2024-12-22 12:48:09 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,64 +1,53 @@
-package employee;
+package package_employee;
 
 import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        EmployeeRepository repository = new InMemoryEmployeeRepository();
+        EmployeeService service = new EmployeeService(repository);
+        Scanner scanner = new Scanner(System.in);
 
-	EmployeeService service=new EmployeeService();
-	static boolean ordering = true;
-	public static void menu() {
-        System.out.println("\n****************Welcome To Employee Managment System *********** "
-        		+ "\n1. Add Employee "
-        		+ "\n2.View Employee"
-        		+ "\n3.Update Employee"
-        		+ "\n4. Delete Employee"
-        		+ "\n5.View All Employee"
-        		+ "\n6. Exit ");
+        while (true) {
+            System.out.println("\n--- Employee Management System ---");
+            System.out.println("1. Add Employee");
+            System.out.println("2. View Employee");
+            System.out.println("3. View All Employees");
+            System.out.println("4. Update Employee");
+            System.out.println("5. Delete Employee");
+            System.out.println("6. Exit");
+            System.out.print("Enter choice: ");
+
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    service.addEmployee();
+                    break;
+                case 2:
+                    System.out.print("Enter Employee ID: ");
+                    int id = scanner.nextInt();
+                    service.viewEmployee(id);
+                    break;
+                case 3:
+                    service.viewAllEmployees();
+                    break;
+                case 4:
+                    System.out.print("Enter Employee ID: ");
+                    id = scanner.nextInt();
+                    service.updateEmployee(id);
+                    break;
+                case 5:
+                    System.out.print("Enter Employee ID: ");
+                    id = scanner.nextInt();
+                    service.deleteEmployee(id);
+                    break;
+                case 6:
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        }
     }
-	 
-	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
-		EmployeeService service=new EmployeeService();
-		
-		do {
-			menu();
-			System.out.println("Enter your Choice");
-			int choice=sc.nextInt();
-			switch(choice) {
-			
-			case 1:
-				System.out.println("Add Employee");
-				service.addEmp();
-				break;
-			case 2:
-				System.out.println("View Employee");
-				service.viewEmp();
-				break;
-			case 3:
-				System.out.println("Update Employee");
-				service.updateEmployee();
-				break;
-			case 4:
-				System.out.println("Delete Employee");
-				service.deleteEmp();
-				break;
-			case 5:
-				System.out.println("view All Employee");
-				service.viewAllEmps("");
-				break;
-			case 6:
-				System.out.println("Thank you for using application!!");
-				System.exit(0);
-				
-			default:
-				System.out.println("Please enter valid choice");
-				break;
-			
-			
-			}
-			
-		}while(ordering);
-		
-			}
-		
-	}
-
+}
